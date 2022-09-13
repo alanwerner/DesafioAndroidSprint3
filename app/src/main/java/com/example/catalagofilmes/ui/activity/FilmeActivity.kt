90309e7a03e3c2ccc1539cbcb7dcc1042c58ff80
alanwerner <alandaswerner@gmail.com>
2022-09-13 20:40:06 -0300

    Tela Criação da tela de detalhes dos filmes

---
+++ app/src/main/java/com/example/catalagofilmes/ui/activity/FilmeActivity.kt
@@ -0,0 +1,12 @@
+package com.example.catalagofilmes.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.catalagofilmes.R
+
+class FilmeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_filme)
+    }
+}
